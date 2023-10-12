84d5fc3bb41f54dcd22392b8363095e94a398d3e
muschellij2 <muschellij2@gmail.com>
2023-10-12 16:21:02 -0400

    updated functions

--- R/create_subject_grid.R
+++ R/create_subject_grid.R
@@ -7,17 +7,19 @@
 #' @param bin_size Size of the bins. `max_signal` must be
 #' evenly divisible by `bin_size`.
 #'
-#' @return
+#' @return A `data.frame` of subject level lags and cut-values
 #' @export
 #'
 #' @examples
 create_subject_predictor = function(
     data,
     max_signal = 3,
-    bin_size = 0.25
-    ) {
-  # time = second = NULL
-  # rm(list = c("time", "second"))
+    bin_size = 0.25,
+    n_lags = 3
+) {
+  cut_lagsig = cut_sig = vm = lag_vm = time = second = NULL
+  rm(list = c("time", "second", "vm", "lag_vm",
+              "cut_lagsig", "cut_sig"))
 
   assertthat::assert_that(
     is.data.frame(data),
@@ -30,12 +32,42 @@ create_subject_predictor = function(
   )
 
   data = data %>%
-    dplyr::mutate(second = lubridate::floor_date(.data[["time"]], "second"))
+    dplyr::mutate(second = lubridate::floor_date(
+      dplyr::.data[["time"]], "second"))
   check = data %>%
-    dplyr::count(.data[["second"]])
+    dplyr::count(dplyr::.data[["second"]])
   assertthat::assert_that(
     any(check$n > 1)
   )
 
+  cell_breaks = seq(0, max_signal, by = bin_size)
+  lags = seq(n_lags)
 
+  result = purrr::map_dfr(lags, .f = function(lag) {
+    out = data %>%
+      dplyr::group_by(second) %>%
+      # for each second, calculate signal and lagged signal
+      dplyr::mutate(lag_vm = dplyr::lag(vm, n = lag)) %>%
+      dplyr::ungroup() %>%
+      dplyr::mutate(
+        cut_sig = cut(
+          vm,
+          breaks = cell_breaks,
+          include.lowest = TRUE
+        ),
+        cut_lagsig = cut(
+          lag_vm,
+          breaks = cell_breaks,
+          include.lowest = TRUE
+        )
+      ) %>%
+      dplyr::filter(!is.na(cut_sig) | !is.na(cut_lagsig))
+    out = out %>%
+      dplyr::count(second, cut_sig, cut_lagsig, .drop = FALSE) %>%
+      dplyr::mutate(
+        lag = lag
+      )
+  })
+
+  return(result)
 }
